3e0edd4a6348b9b530e4ee1f9ae733098e71ae14
Mario Peart <agile.ronin@gmail.com>
2012-10-13 19:40:42 -0400

    Configured for deployment to Heroku

---
+++ foodiexchange/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodiexchange::Application.config.secret_token = '764b100029d2bf90bdcf63f2376777d4a768405b5636389142d13a67358ae89d1479190f767b0376fd16ae20374bb25198db6eff35fd954f8ec3b17fb19cb24e'
